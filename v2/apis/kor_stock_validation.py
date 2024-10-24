1b0b841284ec318548093a60b904a088079d4aeb
thegicode <thegi.code@gmail.com>
2024-10-24 19:05:42 +0900

    update kor_stock_validation.py

--- v2/apis/kor_stock_validation.py
+++ v2/apis/kor_stock_validation.py
@@ -32,12 +32,26 @@ def filter_kor_stocks_by_validation(stocks=None):
         # '종목코드'를 기준으로 stocks에서 없는 종목을 필터링
         df['종목코드'] = df['종목코드'].astype(str)  # 종목코드 형식을 맞춰줌 (필요시)
 
-        # 'X'로만 채워진 열을 확인할 열 목록을 지정합니다. 예: 불성실공시, 관리종목 등
-        # cols_to_check = ['매매거래정지', '정리매매 종목', '관리종목', '투자주의환기종목', '불성실공시']
-
+        # 'X'로만 채워진 열을 확인하여 필터링
         filtered_stocks = df[df.apply(lambda row: (row != 'O').all(), axis=1)]
 
-        return filtered_stocks
+        # '종목코드', '종목명'만 남기고 나머지 열 삭제
+        filtered_stocks = filtered_stocks[['종목코드', '종목명']]
+
+        # stocks 데이터에서 '단축코드'가 일치하는 행을 가져옴
+        stocks['단축코드'] = stocks['단축코드'].astype(str)  # 단축코드 형식을 맞춤
+        merged_stocks = pd.merge(filtered_stocks, stocks, left_on='종목코드', right_on='단축코드', how='inner')
+
+        # 병합된 데이터에서 '종목코드', '종목명', '단축코드' 등 필요한 열만 선택
+        final_stocks = merged_stocks[['종목코드', '표준코드', '종목명', '한글 종목명', '상장일', '시장구분', '주식종류', '액면가', '상장주식수']]
+
+        # 파일 저장
+        csv_path = os.path.join(DATA_KOR_PATH, 'kor_stock_filtered.csv')
+        final_stocks.to_csv(csv_path, index=False, encoding='utf-8-sig')  # 파일 저장 경로 및 인코딩 지정
+
+        print(f"Data saved to {csv_path}, {len(final_stocks)} 종목")
+
+        return final_stocks
 
     except UnicodeDecodeError:
         print("파일을 읽는 중 인코딩 오류가 발생했습니다.")
